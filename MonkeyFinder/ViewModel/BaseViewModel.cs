7a38f5686c82e8fe34f6d5648407eec5ed670f1d
Marcin Smolen <smolen.m@e80group.com>
2023-01-30 10:14:21 +0100

    fix stuff

--- MonkeyFinder/ViewModel/BaseViewModel.cs
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MonkeyFinder.ViewModel
+﻿namespace MonkeyFinder.ViewModel
 {
-    internal class BaseViewModel
+    public partial class BaseViewModel : ObservableObject
     {
+        public BaseViewModel() 
+        {
+            
+        }
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        bool _isBusy;
+        [ObservableProperty]
+        string _title;
+        public bool IsNotBusy => !IsBusy;
     }
 }
